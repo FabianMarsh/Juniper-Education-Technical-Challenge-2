6787c66f33495b87ba3c45c35edc8d6d8a2aa6ef
FabianMarsh <fabian.marsh00@gmail.com>
2025-06-04 13:10:17 +0000

    high cards w/ styles and js

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+document.querySelectorAll(".highlight-card").forEach((card) => {
+    const content = card.querySelector(".highlight-content");
+
+    card.addEventListener("mousemove", (e) => {
+        requestAnimationFrame(() => {
+            const { offsetX, offsetY } = e;
+            const width = card.offsetWidth;
+            const height = card.offsetHeight;
+
+            const rotateX = ((offsetY / height) - 0.5) * 15; 
+            const rotateY = ((offsetX / width) - 0.5) * -15; 
+
+            content.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        });
+    });
+
+    card.addEventListener("mouseleave", () => {
+        requestAnimationFrame(() => {
+            content.style.transform = "rotateX(0deg) rotateY(0deg)";
+        });
+    });
+});
